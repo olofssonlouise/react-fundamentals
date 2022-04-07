af41bcc24b0551fbec4a2a323ecbd7a833683bac
Louise Olofsson <louise.olofsson@forefront.se>
2022-04-07 15:31:04 +0200

    Excercise 5,6&7

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -4,28 +4,60 @@
 import * as React from 'react'
 import '../box-styles.css'
 
-// 🐨 add a className prop to each div and apply the correct class names
-// based on the text content
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-// 💰 each of the elements should have the "box" className applied
-
-// 🐨 add a style prop to each div so their background color
-// matches what the text says it should be
-// 🐨 also use the style prop to make the font italic
-// 💰 Here are available style attributes: backgroundColor, fontStyle
-
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+function Box({style, size, className = '', ...otherProps}){
+  const sizeClassName = size ? `box--${size}` : ''
+  return(
+    <div
+      className={`box ${className} ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="samll" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
+      <Box>sizeless box</Box>
     </div>
   )
 }
+/* first exercise:
+const smallBox = (
+<div
+  className="box box--small"
+  style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+>
+  small lightblue box
+</div>
+)
+
+const mediumBox = (
+<div
+  className="box box--medium"
+  style={{fontStyle: 'italic', backgroundColor: 'pink'}}
+>
+  medium pink box
+</div>
+)
+
+const largeBox = (
+<div
+className="box box--large"
+style={{fontStyle: 'italic', backgroundColor: 'orange'}}
+>
+  large orange box
+</div>
+) */
+
 
 export default App
